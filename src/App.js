fcb5c46470f64501281bc9a4a1168d844e5c28d9
karagooz <batu2919@gmail.com>
2024-01-30 13:52:48 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ColorMatch from './games/colorMatch'
+import MemoryMatch from './games/memoryMatch'
+import RefSample from './RefSample'
+import MemoryMatrix from './games/memoryMatrix'
+
+function App() {
+  return (<>
+    <MemoryMatrix/>
+  </>
+  )
+}
+
+export default App
